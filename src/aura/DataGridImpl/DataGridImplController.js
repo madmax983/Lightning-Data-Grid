b639311adff5e2b46f6cf8f5b5de9c6857be2693
markmasterson <mark.masterson@kenandy.com>
2017-03-16 22:26:49 -0700

    Finishing touches on default decorator. Other Clean-up

--- src/aura/DataGridImpl/DataGridImplController.js
+++ src/aura/DataGridImpl/DataGridImplController.js
@@ -6,63 +6,78 @@
             columns: [
                 {
                     name: "name",
-                    label: "Name"
+                    label: "Name",
+                    type: "text"
                 },
                 {
                     name: "title",
-                    label: "Title"
+                    label: "Title",
+                    type: "text"
                 },
                 {
                     name: "phone",
-                    label: "Phone"
+                    label: "Phone",
+                    type: "phone"
                 },
                 {
                     name: "home_phone",
-                    label: "Home Phone"
+                    label: "Home Phone",
+                    type: "phone"
                 },
                 {
                     name: "mobile",
-                    label: "Mobile"
+                    label: "Mobile",
+                    type: "phone"
                 },
                 {
                     name: "other_phone",
-                    label: "Other Phone"
+                    label: "Other Phone",
+                    type: "phone"
                 },
                 {
                     name: "fax",
-                    label: "Fax"
+                    label: "Fax",
+                    type: "phone"
                 },
                 {
                     name: "reports_to",
-                    label: "Reports To"
+                    label: "Reports To",
+                    type: "text"
                 },
                 {
                     name: "email",
-                    label: "Email"
+                    label: "Email",
+                    type: "email"
                 },
                 {
                     name: "industry",
-                    label: "Industry"
+                    label: "Industry",
+                    type: "text"
                 },
                 {
                     name: "mailing_address",
-                    label: "Mailing Address"
+                    label: "Mailing Address",
+                    type: "text"
                 },
                 {
                     name: "other_address",
-                    label: "Other Address"
+                    label: "Other Address",
+                    type: "text"
                 },
                 {
                     name: "languages",
-                    label: "Languages"
+                    label: "Languages",
+                    type: "text"
                 },
                 {
                     name: "assistant",
-                    label: "Assistant"
+                    label: "Assistant",
+                    type: "text"
                 },
                 {
                     name: "level",
-                    label: "Level"
+                    label: "Level",
+                    type: "number"
                 }
             ],
 
@@ -73,25 +88,26 @@
 
         var data = [];
 
-        for(var i = 0; i < 10000; i++) {
+        for(var i = 0; i < 99; i++) {
+            var phone = i < 10 ? 10 : 0;
             var newDataItem = {
                 data: {
                     id: "Id-" + i,
                     name: "Contact Name " + i,
                     title: "Contact Title " + i,
-                    phone: "Contact Phone" + i,
-                    home_phone: "Contact Home Phone" + i,
-                    mobile: "Contact Mobile Phone" + i,
-                    other_phone: "Contact Other Phone" + i,
-                    fax: "Contact Fax " + i,
+                    phone: "(650) 812-58" + phone + i,
+                    home_phone: "(650) 932-54" + phone + i,
+                    mobile: "(650) 932-53" + phone + i,
+                    other_phone: "(650) 932-46" + phone + i,
+                    fax: "(650) 932-98" + phone + i,
                     reports_to: "Contact Reports To " + i,
-                    email: "Contact Email " + i,
+                    email: "contactemail" + i + "@contact.com",
                     industry: "Contact Industry " + i,
                     mailing_address: "Contact Mailing Address " + i,
                     other_address: "Contact Other Address " + i,
                     languages: "Contact Languages " + i,
                     assistant: "Contact Assistant " + i,
-                    level: "Contact Level " + i
+                    level: i
                 },
                 parent: null,
                 hasChildren: false,
@@ -101,25 +117,25 @@
             data.push(newDataItem);
         }
 
-        for(var j = 0; j < 5000; j++) {
+        for(var j = 0; j < 33; j++) {
             var subDataItem = {
                 data: {
                     id: "Id-Sub-" + j,
                     name: "Sub Contact Name " + j,
                     title: "Sub Contact Title " + j,
-                    phone: "Sub Contact Phone" + j,
-                    home_phone: "Sub Contact Home Phone" + j,
-                    mobile: "Sub Contact Mobile Phone" + j,
-                    other_phone: "Sub Contact Other Phone" + j,
-                    fax: "Sub Contact Fax " + j,
-                    reports_to: "Sub Contact Reports To " + j,
-                    email: "Sub Contact Email " + j,
+                    phone: "(650) 812-58" + phone + j,
+                    home_phone: "(650) 932-54" + phone + j,
+                    mobile: "(650) 932-53" + phone + j,
+                    other_phone: "(650) 932-46" + phone + j,
+                    fax: "(650) 932-98" + phone + j,
+                    reports_to: " Sub Contact Reports To " + j,
+                    email: "subcontactemail" + j + "@contact.com",
                     industry: "Sub Contact Industry " + j,
                     mailing_address: "Sub Contact Mailing Address " + j,
                     other_address: "Sub Contact Other Address " + j,
                     languages: "Sub Contact Languages " + j,
                     assistant: "Sub Contact Assistant " + j,
-                    level: "Sub Contact Level " + j
+                    level: j
                 },
                 parent: "Id-" + j * 2,
                 hasChildren: false,
@@ -134,19 +150,19 @@
                 id: "Id-Sub-Sub-" + j,
                 name: "Sub Sub Contact Name " + j,
                 title: "Sub Sub Contact Title " + j,
-                phone: "Sub Sub Contact Phone" + j,
-                home_phone: "Sub Sub Contact Home Phone" + j,
-                mobile: "Sub Sub Contact Mobile Phone" + j,
-                other_phone: "Sub Sub Contact Other Phone" + j,
-                fax: "Sub Sub Contact Fax " + j,
+                phone: "(650) 812-58" + phone + j,
+                home_phone: "(650) 812-58" + phone + j,
+                mobile: "(650) 812-58" + phone + j,
+                other_phone: "(650) 812-58" + phone + j,
+                fax: "(650) 812-58" + phone + j,
                 reports_to: "Sub Sub Contact Reports To " + j,
-                email: "Sub Sub Contact Email " + j,
+                email: "subsubcontactemail" + j + "@contact.com",
                 industry: "Sub Sub Contact Industry " + j,
                 mailing_address: "Sub Sub Contact Mailing Address " + j,
                 other_address: "Sub Sub Contact Other Address " + j,
                 languages: "Sub Sub Contact Languages " + j,
                 assistant: "Sub Sub Contact Assistant " + j,
-                level: "Sub Sub Contact Level " + j
+                level: j
             },
             parent: "Id-Sub-0",
             hasChildren: false,
