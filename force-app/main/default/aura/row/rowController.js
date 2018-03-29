f589b68631286eda78996391b0be66bc0cd3ff6b
mmasterson <markmasterson@gmail.com>
2018-03-28 19:03:47 -0500

    #46 Added Child Toggle to Row

--- force-app/main/default/aura/row/rowController.js
+++ force-app/main/default/aura/row/rowController.js
@@ -2,5 +2,9 @@
     doInit: function(component, event) {
         var rowData = component.get("v.rowData");
         component.set("v.childrenVisible", rowData.expanded);
+    },
+    childrenToggle: function(component) {
+        var cells = component.find("cells");
+        cells[0].toggleChildren();
     }
 })
